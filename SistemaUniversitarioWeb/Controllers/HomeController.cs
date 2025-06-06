d3c69e66e2f11d3afe32b7fb7f88013f20bd6a26
Daniel Leon <danie123975@gmail.com>
2025-06-05 19:30:09 -0500

    Agregar archivos de proyecto.

---
+++ SistemaUniversitarioWeb/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using SistemaUniversitarioWeb.Models;
+using System.Diagnostics;
+
+namespace SistemaUniversitarioWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ActionResult Pregrados()
+        {
+            return View();
+        }
+
+        public ActionResult IngenieriaCivil()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SistemaUniversitarioWeb/Views/Home/Pregrados.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SistemaUniversitarioWeb.Views.Home
+{
+    public class PregradosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
